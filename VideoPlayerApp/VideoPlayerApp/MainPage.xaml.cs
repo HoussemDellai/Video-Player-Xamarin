a430f55ee5805a4ee2173b2587e5083f5b08e8bf
Houssem Dellai <houssem.dellai@live.com>
2017-11-12 10:48:54 +0100

    Add project files.

---
+++ VideoPlayerApp/VideoPlayerApp/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Plugin.MediaManager;
+using Plugin.MediaManager.Abstractions.Enums;
+using Xamarin.Forms;
+
+namespace VideoPlayerApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        
+        private async void PlayStop_Clicked(object sender, EventArgs e)
+        {
+            if (PlayPauseButton.Text == "Play")
+            {
+                await CrossMediaManager.Current.Play(VideoEntry.Text, MediaFileType.Video);
+
+                PlayPauseButton.Text = "Stop";
+            }
+
+            else if (PlayPauseButton.Text == "Stop")
+            {
+                await CrossMediaManager.Current.Stop();
+
+                PlayPauseButton.Text = "Play";
+            }
+        }
+    }
+}
